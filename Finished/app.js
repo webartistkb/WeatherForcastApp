5b4a307f957a5a4e4ee9a078181b23db0d0f756a
Kamal Bhatia <bhatia.kamal@outlook.com>
2015-11-29 17:47:58 -0500

    added more stuff

--- Finished/app.js
+++ Finished/app.js
@@ -17,12 +17,20 @@ weatherApp.config(function ($routeProvider) {
     })
     
 });
+// SERVICES
+weatherApp.service('cityService',function(){
+   this.city = "New York, NY"; 
+});
 
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', function($scope) {
+weatherApp.controller('homeController',['$scope', 'cityService', function($scope,cityService) {
+    $scope.city = cityService.city;
     
+    $scope.$watch('city', function(){
+       cityService.city = $scope.city; 
+    });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
-    
+weatherApp.controller('forecastController', ['$scope','cityService', function($scope,cityService) {
+    $scope.city = cityService.city;
 }]);
