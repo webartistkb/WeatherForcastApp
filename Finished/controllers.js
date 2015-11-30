a7bed11fc4f58b055e258aa7d3b7d474b2785083
Kamal Bhatia <bhatia.kamal@outlook.com>
2015-11-29 20:27:01 -0500

    FINAL UPDATE

--- Finished/controllers.js
+++ Finished/controllers.js
@@ -1,35 +1,3 @@
-// MODULE
-var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
-
-// ROUTES
-weatherApp.config(function ($routeProvider) {
-   
-    $routeProvider
-    
-    .when('/', {
-        templateUrl: 'pages/home.htm',
-        controller: 'homeController'
-    })
-    
-    .when('/forecast', {
-        templateUrl: 'pages/forecast.htm',
-        controller: 'forecastController'
-    })
-    
-    .when('/forecast/:days', {
-        templateUrl: 'pages/forecast.htm',
-        controller: 'forecastController'
-    })
-    
-});
-
-// SERVICES
-weatherApp.service('cityService', function() {
-   
-    this.city = "New York, NY";
-    
-});
-
 // CONTROLLERS
 weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
     
